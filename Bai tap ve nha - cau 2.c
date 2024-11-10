d3510f776fc6dfdbaee1f7aec68bdebd5003c83c
Đặng Huyền Anh <139437665+huyenanhdang@users.noreply.github.com>
2024-11-10 23:32:49 +0700

    Update Bai tap ve nha - cau 2.c

--- Bai tap ve nha - cau 2.c
+++ Bai tap ve nha - cau 2.c
@@ -3,12 +3,27 @@
 
 int main()
 {
-    int i, n, j;
+    int i, n;
+    double tong = 0.0;
     printf("Hay nhap gia tri cua n vao day: ");
     scanf("%d", &n);
 
     for (i = 1; i <= n; i++)
     {
-        
+        if (i % 2 == 1)
+        {
+            tong += 1.0 /i;
+        }
+        else 
+        {
+            tong -= 1.0 / i;
+        }
     }
+    printf("Tong cac so la: %.4f\n", tong);
+    return(0);
 }
+
+
+
+
+
